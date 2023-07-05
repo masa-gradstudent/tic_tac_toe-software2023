0be220483b0425887e5d9f731a81d173e96ec260
upnt <upnt.github@gmail.com>
2023-07-05 02:17:26 +0000

    create drawing box

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
+import "fmt"
+
 type Board struct {
 	tokens [3][3]string
 }
 
 func create_board() Board {
 	b := Board{}
-	b.tokens = [3][3]string{{"", "", ""}, {"", "", ""}, {"", "", ""}}
+	b.tokens = [3][3]string{{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}}
 	return b
 }
 
@@ -33,5 +35,22 @@ func (b *Board) check_winner() string {
 	if b.get(0, 2) == b.get(1, 1) && b.get(1, 1) == b.get(2, 0) {
 		return b.get(0, 2)
 	}
-	return ""
+	return " "
+}
+
+func (b *Board) print() {
+	fmt.Printf("-------\n")
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Printf("|%s", b.get(i, j))
+		}
+		fmt.Printf("|\n")
+		fmt.Printf("-------\n")
+	}
+}
+
+func main() {
+	b := create_board()
+	b.put(0, 1, "o")
+	b.print()
 }
